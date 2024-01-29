fb3f29ef8f2e3fc5d8fb5efe717d8f70d184beaf
Andrés Fulla <andres.fulla@producthackers.com>
2024-01-29 10:15:04 +0100

    fix: plugins the right way

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,14 +2,4 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {
- -- Copilot
-  'github/copilot.vim',
-  config = function()
-    -- Copilot Remap
-      vim.g.copilot_no_tab_map = true
-      vim.g.copilot_assume_mapped = true
-      vim.api.nvim_set_keymap('i', '<C-ENTER>', 'copilot#Accept("<CR>")', {expr=true, silent=true})
-  end,
-  'rust-lang/rust.vim'
-}
+return {}
